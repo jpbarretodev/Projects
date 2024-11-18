2e9f54a507925359444679b046b0057f7cd33b26
jpbarretodev <jbarretodasilva7@gmail.com>
2024-11-17 21:46:43 -0700

    update

--- python/assistente_tarefas/Assistente_de_Tarefas.py
+++ python/assistente_tarefas/Assistente_de_Tarefas.py
@@ -1,4 +1,5 @@
 #incluir biblioteca caso necessidade
+import json
 pass
 
 #globais
@@ -14,9 +15,9 @@ class Tarefa:
     def adicionar_tarefa(self):
         try:
             objetos.append(self)
-            return print("Tarefa adicionada com sucesso")
+            return print("\nTarefa adicionada com sucesso!\n")
         except:
-            return print("Não foi possível adicionar a tarefa")
+            return print("Não foi possível adicionar a tarefa\n")
 
     @staticmethod
     def objeto_unico(index):
@@ -27,7 +28,7 @@ class Tarefa:
     def visualizar_tarefa():
         if len(objetos) > 0:
             for i, objeto in enumerate(objetos):
-                print(f"{i+1} - {objeto.nome}: {objeto.tempo} horas")
+                print(f"\n{i+1} - {objeto.nome}: {objeto.tempo} horas")
 
     @staticmethod
     def excluir_tarefa():
@@ -36,9 +37,9 @@ class Tarefa:
         escolha = int(input("Selecione o número da tarefa que deseja excluir.\nEscolha: "))
         if escolha != 0 and escolha <= len(objetos):
             objetos.pop(escolha-1)
-            return print("Tarefa apagada com sucesso.")
+            return print("Tarefa apagada com sucesso\n")
         else:
-            return print("Falha ao excluir objeto!")
+            return print("Falha ao excluir objeto.")
         
     @staticmethod
     def editar_tarefa():
@@ -53,10 +54,10 @@ class Tarefa:
                 objeto_unico = Tarefa.objeto_unico(verificacao-1)
                 if escolha == 1:
                     objeto_unico.nome = input("Insira o novo nome: ")
-                    return print("O nome da tarefa foi alterado com sucesso!")
+                    return print("O nome da tarefa foi alterado com sucesso!\n")
                 else:
                     objeto_unico.tempo = int(input("Insira o novo tempo para a tarefa: "))
-                    return print("O tempo da tarefa foi alterado com sucesso!")
+                    return print("O tempo da tarefa foi alterado com sucesso!\n")
                 
     @staticmethod
     def verificacao_lista():
@@ -65,17 +66,13 @@ class Tarefa:
         else:
             return True
 
-    @staticmethod
-    def criar_documento():
-        pass
-
-class Metodos:
+class Metodos: #classe criada para organizar as funções que serão usadas
 
     @staticmethod
-    def primeira_acesso(verificacao):       #faz a verificação da lista para o primeiro acesso (ideal para usar depois com o .txt)
+    def primeira_acesso(verificacao):       #faz a verificação da lista para o primeiro acesso 
         if verificacao == False:
-            escolha = int(input("Você não possui nenhuma tarefa\n1 - Adicionar tarefa\n0 - Fechar programa\nEscolha: "))
-            while escolha != 1 and escolha != 0:
+            escolha = int(input("Você não possui nenhuma tarefa\n1 - Adicionar tarefa\n2 - Restaurar tarefas\n0 - Fechar programa\nEscolha: "))
+            while escolha not in [0, 1, 2]:
                 escolha = int(input("Opção inválida. Por favor, insira novamente.\nEscolha: "))
             else:
                 if escolha == 0:
@@ -83,13 +80,33 @@ class Metodos:
                 elif escolha == 1:
                     nome, tempo = input("insira o nome da tarefa: "), int(input("Insira o tempo para realizar a tarefa: "))
                     Tarefa.adicionar_tarefa(Tarefa(nome, tempo))
+                elif escolha == 2:
+                    Metodos.restaurar()
     
         else:
-            funcionalidade = int(input("Insira a opção desejada:\n1 - Adicionar tarefa\n2 - visualizar tarefas\n3 - Excluir tarefa\n4 - Editar tarefa\n0 - Encerrar programa\nEscolha: "))
+            funcionalidade = int(input("\nInsira a opção desejada:\n1 - Adicionar tarefa\n2 - visualizar tarefas\n3 - Excluir tarefa\n4 - Editar tarefa\n5 - Salvar tarefas\n0 - Encerrar programa\nEscolha: "))
             while funcionalidade < 0 and funcionalidade > 4:
-                funcionalidade = int(input("Opção inválida. Insira novamente\nEscolha: "))
+                funcionalidade = int(input("\nOpção inválida. Insira novamente\nEscolha: "))
             else:
                 return funcionalidade           #retorna a escolha da funcionalidade pelo usuário
+
+    @staticmethod
+    def salvar():
+        for i in range(len(objetos)):
+            dicio = {}
+            objeto_tarefa  = Tarefa.objeto_unico(i)
+            dicio[f"{objeto_tarefa.nome}"] = objeto_tarefa.tempo
+            #print(dicio)
+            with open("Projects/Python/assistente_tarefas/tarefas.json", "w") as arq:
+                json.dump(dicio, arq)
+
+    @staticmethod
+    def restaurar():
+        with open("Projects/Python/assistente_tarefas/tarefas.json") as arq:
+            arq = json.load(arq)
+        for i, j in arq.items():
+            Tarefa.adicionar_tarefa(Tarefa(i, j))
+        pass
             
 def main():
 
@@ -105,6 +122,10 @@ def main():
             Tarefa.excluir_tarefa()
         elif funcionalidade_escolhida == 4:
             Tarefa.editar_tarefa()
+        elif funcionalidade_escolhida == 5:
+            Metodos.salvar()
+        elif funcionalidade_escolhida == 6:
+            Metodos.restaurar()
         elif funcionalidade_escolhida == 0:
             print("Programa Encerrado")
             break
